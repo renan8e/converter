b3118be9c02f597420fd496bddd7abeaf208319e
Renan Gabriel <126295397+renan8e@users.noreply.github.com>
2023-07-18 17:41:11 -0300

    hexu-getLine-s1

--- index.c
+++ index.c
@@ -1,16 +1,57 @@
 #include <stdio.h>
+
 #define MAXLINE 50
+enum {NO = 0,YES = 1};
 /* Convert Case */
+
 char line[MAXLINE];
 
-//void getLine(void);
-
+void hexu(void);
 void minus(void);
+void getLine(void);
 
 int main(){
-
-	minus();
+	hexu();
 }
+void hexu(void){
+
+	getLine();
+
+	extern char line[];
+
+	int hexdigit,i,inhex,n;
+	n = i = 0 ;
+	inhex = YES;
+
+	while (inhex == YES)
+	{
+		if (line[i] >= '0' && line[i] <= '9')
+		{
+			hexdigit = line[i] - '0';
+		}
+		else if (line[i] >= 'a' && line[i] <= 'z')
+		{
+			hexdigit = line[i] - 'a' + 10;
+		}
+		else if (line[i] >= 'A' && line[i] <= 'Z')
+		{
+			hexdigit = line[i] - 'A' + 10;
+		}
+		else
+		{
+			inhex = NO;
+		}
+
+		if (inhex == YES)
+			n = 16 * n + hexdigit ;
+		++i;
+	}
+
+//	printf("%d\n",n);
+}
+
+
+
 void minus(void)// input a line and output it in Lowercase 
 {
 	int c;
@@ -28,8 +69,7 @@ void minus(void)// input a line and output it in Lowercase
 	printf("\n");
 }
 
-/*void getLine(void){ // get line into a string
-
+void getLine(void){ // get line into a string
 	int c,i;
 	extern char line[];
 	i = 0;
@@ -42,4 +82,4 @@ void minus(void)// input a line and output it in Lowercase
 	}
 
 	line[i] = '\0';
-}*/
+}
